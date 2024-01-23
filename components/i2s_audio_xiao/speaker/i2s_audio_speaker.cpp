7ebe07ea3cb25bd7f666b538bf26c6e5bd947762
JoJang2u <47235085+QingWind6@users.noreply.github.com>
2024-01-23 09:50:13 +0800

    Update i2s_audio_speaker.cpp
    change i2s mode

--- components/i2s_audio_xiao/speaker/i2s_audio_speaker.cpp
+++ components/i2s_audio_xiao/speaker/i2s_audio_speaker.cpp
@@ -40,7 +40,7 @@ void I2SAudioSpeaker::player_task(void *params) {
   xQueueSend(this_speaker->event_queue_, &event, portMAX_DELAY);
 
   i2s_driver_config_t config = {
-      .mode = (i2s_mode_t) (I2S_MODE_MASTER | I2S_MODE_TX),
+      .mode = (i2s_mode_t) (I2S_MODE_SLAVE | I2S_MODE_TX),
       .sample_rate = 16000,
       .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
       .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
@@ -50,8 +50,8 @@ void I2SAudioSpeaker::player_task(void *params) {
       .dma_buf_len = 128,
       .use_apll = false,
       .tx_desc_auto_clear = true,
-      .fixed_mclk = I2S_PIN_NO_CHANGE,
-      .mclk_multiple = I2S_MCLK_MULTIPLE_DEFAULT,
+      // .fixed_mclk = I2S_PIN_NO_CHANGE,
+      // .mclk_multiple = I2S_MCLK_MULTIPLE_DEFAULT,
       .bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT,
   };
 #if SOC_I2S_SUPPORTS_DAC
